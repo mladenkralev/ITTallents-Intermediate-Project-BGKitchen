e06c5d3fe81cdbe9bf24a2cefb15d250970069d9
yordanstoichkov <slav4o95@gmail.com>
2016-09-20 10:21:13 +0300

    Database fill

--- test/MealTest.java
+++ test/MealTest.java
@@ -2,21 +2,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import food.ingredient.IngredentException;
+import food.ingredient.Ingredient;
+import food.ingredient.IngredientDAO;
 import food.meal.Meal;
 import food.meal.MealDAO;
 import food.meal.MealException;
 
 public class MealTest {
+	private MealDAO mealTest = new MealDAO();
+	private IngredientDAO ingredientTest = new IngredientDAO();
 
 	@Test
 	public void test() throws MealException {
-		Meal meal = new Meal("Nadenicata", false, "asd.jpg");
-		meal.setVegan(false);
-		
-		int id = new MealDAO().addMeal(meal);
-		
-		assertNotEquals(id, 0);
-		
+		try {
+			Meal banica = new Meal("Таратор", true, "dish/tarator.jpg");
+
+			Ingredient kori = new Ingredient("Кисело мляко", 1f);
+			Ingredient sirene = new Ingredient("краставица", 1f);
+
+			ingredientTest.addIngredient(sirene);
+			ingredientTest.addIngredient(kori);
+
+			banica.addIngredients(kori);
+			banica.addIngredients(sirene);
+
+			int id = new MealDAO().addMeal(banica);
+
+			assertNotEquals(id, 0);
+		} catch (IngredentException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 	}
 
 }
